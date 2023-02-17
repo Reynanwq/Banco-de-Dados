23ad0227c42be9fed2c663c084bc448bc3bb22f7
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-17 18:54:55 -0300

    deletando dados de uma tabela

---
+++ SQL Questoes/questao 02.sql
@@ -0,0 +1,20 @@
+-- QUESTÃO 02: DELETAR A TABELA e CRIANDO UMA TABELA
+
+DROP TABLE pessoas;
+
+CREATE TABLE pessoas (
+	Nome VARCHAR(50) NOT NULL PRIMARY KEY,
+    Cpf INT NOT NULL,
+	Ano INT NOT NULL,
+    Mes INT NOT NULL
+);
+
+INSERT INTO pessoas (Nome, Cpf, Ano, Mes) 
+VALUES 
+('Reynan', 05123687, 2003, 08);
+('Pedro', 45646, 2001, 09),
+('Vinicius', 12478, 2002, 10),
+('Keio', 96545, 2000, 03)	
+
+
+SELECT * FROM pessoas;
